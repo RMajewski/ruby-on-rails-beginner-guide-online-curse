50003f5a23ebbba4f9f7ff66c4361e187066362d
Rene Majewski <rene.majewski@gmx.de>
2017-08-30 11:53:26 +0000

    Lektion 17.2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,25 @@
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(params[:comment].permit(:body))
+    @comment.user_id = current_user.id if current_user
+    
+    if @comment.save
+      redirect_to article_path(@article), notice: "Your comment has been saved."
+    else
+      redirect_to 'new'
+    end
+  end
   
+  def update
+    @article = Article.find(params[:article_id])
+  end
+  
+  def edit
+    @article = Article.find(params[:article_id])
+  end
+  
+  def destroy
+    @article = Article.find(params[:article_id])
+  end
 end
